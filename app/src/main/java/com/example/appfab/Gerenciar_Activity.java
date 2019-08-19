020604992565f4585210eb2f72374fa1dd1e2633
aniely.osilva <anielysilva08@gmail.com>
2019-08-19 15:11:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appfab/Gerenciar_Activity.java
@@ -0,0 +1,14 @@
+package com.example.appfab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gerenciar_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gerenciar_layout);
+    }
+}
---
+++ app/src/main/java/com/example/appfab/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.appfab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton btnfba;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnfba = (FloatingActionButton) findViewById(R.id.idFAB);
+
+        btnfba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Gerenciar_Activity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
